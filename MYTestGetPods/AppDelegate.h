907de6899482c72e16804996c1b28e81efd49d37
yemingyu <334111028@qq.com>
2016-02-25 20:30:03 +0800

    add files

---
+++ MYTestGetPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYTestGetPods
+//
+//  Created by yemingyu on 2/15/16.
+//  Copyright © 2016 yemingyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+
+source "https://github.com/CocoaPods/Specs.git"
+source "git@bitbucket.org:mingyu_ye/myprivatepodsymy.git"
+
+
+platform:ios, '8.0'
+
+pod 'MYNewPrivatePod', '0.0.1'
+pod 'MYCocoaPodsTestYMY', '0.0.1'
+
+pod 'JSPatch', '~> 0.1.3'
